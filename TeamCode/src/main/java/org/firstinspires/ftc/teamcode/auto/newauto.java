5b3d17c94e9f6a51fc8e8d8a575e820d9f1a1324
rachit-j <56803677+rachit-j@users.noreply.github.com>
2022-11-19 21:48:09 -0800

    Update newauto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/newauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/newauto.java
@@ -36,6 +36,7 @@ import android.view.View;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
@@ -52,7 +53,7 @@ import java.util.Locale;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Sensor: REVColorDistance", group = "Sensor")
+@Autonomous(name = "Color Sensor / Autonomous", group = "Competiton")
                             // Comment this out to add to the opmode list
 public class newauto extends LinearOpMode {
 
