91ca4374c8f533d50c6aeedd2890ea61476d568a
rachit-j <rjaiswal.sd.77@gmail.com>
2023-02-21 21:10:34 -0800

    Woodright

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/WOODrightcamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/WOODrightcamera.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import java.util.ArrayList;
 
 @Autonomous
-public class WOODright extends LinearOpMode {
+public class WOODrightcamera extends LinearOpMode {
 
     OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
@@ -143,121 +143,254 @@ public class WOODright extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new org.firstinspires.ftc.teamcode.auto.AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
 
-        //motors
-        fl = hardwareMap.get(DcMotor.class, "fl");
-        fr = hardwareMap.get(DcMotor.class, "fr");
-        bl = hardwareMap.get(DcMotor.class, "bl");
-        br = hardwareMap.get(DcMotor.class, "br");
-        lift1 = hardwareMap.get(DcMotor.class, "lift1");
-        lift2 = hardwareMap.get(DcMotor.class, "lift2");
-        arm = hardwareMap.get(DcMotor.class, "arm");
-
-        //servos
-        claw = hardwareMap.get(Servo.class, "claw");
-        wrist = hardwareMap.get(Servo.class, "wrist");
-
-        //odometers
-        verticalLeft = hardwareMap.dcMotor.get("fl");
-        verticalRight = hardwareMap.dcMotor.get("br");
-        horizontal = hardwareMap.dcMotor.get("fr");
-
-        //robot hardware
-        RobotHardware robot = new RobotHardware(fl, fr, bl, br, lift1, lift2, arm);
-        robot.innitHardwareMap();
-
-        imuinit();
-        sleep(500);
-
-        telemetry.addData(">", "Press Play to start op mode");
-        telemetry.addData("angle", getAngle());
-        telemetry.update();
-
-        waitForStart();
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        //start odometry thread
-        update = new odometry(verticalLeft, verticalRight, horizontal, 10, imu);
-        Thread positionThread = new Thread(update);
-        positionThread.start();
-
-        int[] armHeight = {360, 270, 180, 90, 0};
-        resetRuntime();
-
-        //grab cone
-        clawClose();
-        sleep(300);
-        robot.setArm(600, 0.8);
-        sleep(200);
-        wristTurn();
-        moveTo(-16, 0, -90, 3);
-
-        //drive to pole
-        robot.setLift(850, 1);
-        moveTo(-50, -3, -90, 3);
-
-        //align with pole
-        runtime.reset();
-        while (runtime.seconds() < 1 && opModeIsActive()) {
-            stay(-60, -8, -45);
-        }
-        runtime.reset();
-        while (runtime.seconds() < 0.3 && opModeIsActive()) {
-            clawOpen();
-        }
-
-        //start of 5 cycles
-        for (int i = 0; i < 5; i++){
-            alignwithconestack();
-
-            runtime.reset();
-            while (runtime.seconds() < 1.4 && opModeIsActive()) {
-                movetoconestack();
-                robot.setLift(armHeight[i], 0.5);
-                robot.setArm(1, 0.5);
-                wristReset();
-                clawOpen();
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
             }
 
-            runtime.reset();
-            while (runtime.seconds() < 0.5 && opModeIsActive()) {
-                clawClose();
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+        boolean tag1Found = false;
+        boolean tag2Found = false;
+        boolean tag3Found = false;
+
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested()) {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if (currentDetections.size() != 0) {
+
+
+                for (AprilTagDetection tag : currentDetections) {
+                    if (tag.id == 9) {
+                        tagOfInterest = tag;
+                        tag1Found = true;
+                        break;
+                    }
+
+                    if (tag.id == 11) {
+                        tagOfInterest = tag;
+                        tag2Found = true;
+                        break;
+                    }
+
+                    if (tag.id == 18) {
+                        tagOfInterest = tag;
+                        tag3Found = true;
+                        break;
+                    }
+                }
+
+                if (tag1Found) {
+                    telemetry.addLine("Tag 1 Located!");
+                    tagToTelemetry(tagOfInterest);
+                } else if (tag2Found) {
+                    telemetry.addLine("Tag 2 Located!");
+                    tagToTelemetry(tagOfInterest);
+                } else if (tag3Found) {
+                    telemetry.addLine("Tag 3 Located!");
+                    tagToTelemetry(tagOfInterest);
+                } else {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if (tagOfInterest == null) {
+                        telemetry.addLine("(The tag has never been seen)");
+                    } else {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            } else {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if (tagOfInterest == null) {
+                    telemetry.addLine("(The tag has never been seen)");
+                } else {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
             }
 
-            robot.setLift(850, 1);
+            telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if (tagOfInterest != null) {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        } else {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+        /* Actually do something useful */
+        if (tagOfInterest == null) {
+            /*
+             * Insert your autonomous code here, presumably running some default configuration
+             * since the tag was never sighted during INIT
+             */
+        } else {
+            //motors
+            fl = hardwareMap.get(DcMotor.class, "fl");
+            fr = hardwareMap.get(DcMotor.class, "fr");
+            bl = hardwareMap.get(DcMotor.class, "bl");
+            br = hardwareMap.get(DcMotor.class, "br");
+            lift1 = hardwareMap.get(DcMotor.class, "lift1");
+            lift2 = hardwareMap.get(DcMotor.class, "lift2");
+            arm = hardwareMap.get(DcMotor.class, "arm");
+
+            //servos
+            claw = hardwareMap.get(Servo.class, "claw");
+            wrist = hardwareMap.get(Servo.class, "wrist");
+
+            //odometers
+            verticalLeft = hardwareMap.dcMotor.get("fl");
+            verticalRight = hardwareMap.dcMotor.get("br");
+            horizontal = hardwareMap.dcMotor.get("fr");
+
+            //robot hardware
+            RobotHardware robot = new RobotHardware(fl, fr, bl, br, lift1, lift2, arm);
+            robot.innitHardwareMap();
+
+            imuinit();
+            sleep(500);
+
+            telemetry.addData(">", "Press Play to start op mode");
+            telemetry.addData("angle", getAngle());
+            telemetry.update();
+
+            waitForStart();
+            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            lift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            lift2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            //start odometry thread
+            update = new odometry(verticalLeft, verticalRight, horizontal, 10, imu);
+            Thread positionThread = new Thread(update);
+            positionThread.start();
+
+            int[] armHeight = {360, 270, 180, 90, 0};
+            resetRuntime();
+
+            //grab cone
+            clawClose();
+            sleep(300);
             robot.setArm(600, 0.8);
-
-            movetopole();
+            sleep(200);
             wristTurn();
+            moveTo(-16, 0, -90, 3);
 
+            //drive to pole
+            robot.setLift(850, 1);
+            moveTo(-50, -3, -90, 3);
+
+            //align with pole
             runtime.reset();
             while (runtime.seconds() < 1 && opModeIsActive()) {
-                alignwithpole();
+                stay(-60, -8, -45);
             }
-
             runtime.reset();
-            while (runtime.seconds() < 0.2 && opModeIsActive()) {
-                alignwithpole();
+            while (runtime.seconds() < 0.3 && opModeIsActive()) {
                 clawOpen();
             }
 
+            //start of 5 cycles
+            for (int i = 0; i < 5; i++) {
+                alignwithconestack();
+
+                runtime.reset();
+                while (runtime.seconds() < 1.4 && opModeIsActive()) {
+                    movetoconestack();
+                    robot.setLift(armHeight[i], 0.5);
+                    robot.setArm(1, 0.5);
+                    wristReset();
+                    clawOpen();
+                }
+
+                runtime.reset();
+                while (runtime.seconds() < 0.5 && opModeIsActive()) {
+                    clawClose();
+                }
+
+                robot.setLift(850, 1);
+                robot.setArm(600, 0.8);
+
+                movetopole();
+                wristTurn();
+
+                runtime.reset();
+                while (runtime.seconds() < 1 && opModeIsActive()) {
+                    alignwithpole();
+                }
+
+                runtime.reset();
+                while (runtime.seconds() < 0.2 && opModeIsActive()) {
+                    alignwithpole();
+                    clawOpen();
+                }
+
+            }
+
+
+            //parking
+            moveTo(-50, -3, 0, 1);
+            clawClose();
+            wristReset();
+            robot.setLift(0, 0.5);
+            robot.setArm(0, 0.3);
+
+            if(tag1Found == true) {
+                // Enter park code here
+                moveTo(-50, -24, 0, 2);
+
+            } else if(tag2Found == true) {
+                // Enter park code here
+                while (runtime.seconds() < 2) {
+                    stay(-50, -2, 0);
+                }
+            } else if(tag3Found == true) {
+                moveTo(-50, 24, 0, 2);
+            } else {
+                moveTo(-50, 24, 0, 2);
+            }
+
+            update.stop();
+            stop();
         }
 
 
-        //parking
-        moveTo(-50, -3, 0, 1);
-        clawClose();
-        wristReset();
-        robot.setLift(0, 0.5);
-        robot.setArm(0, 0.3);
-
-        update.stop();
-        stop();
-
-
-
+    }
+    void tagToTelemetry(AprilTagDetection detection) {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x * FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y * FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z * FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
     }
 
     public void alignwithconestack() {
